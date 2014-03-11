9858e70717d9682d2383ebf136d1fa5ead2f8888
Sean Herron <seanherron@gmail.com>
2014-03-11 01:37:42 -0400

    added loader

--- recalls/static/js/site.js
+++ recalls/static/js/site.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-
 	if (navigator.userAgent.match(/Android/i)) {
 		$( ".barcode-button" ).hide();
-		$( ".barcode-button a" ).click();
+		$( ".loading" ).show();
+		
+		window.setTimeout(function() {
+		location.href = document.getElementsByClassName("barcode-button")[0].getElementsByTagName("a")[0].href;
+		}, 500);
 	}
 
 });
