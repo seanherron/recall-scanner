173bd7db98d246a8987c8a37d44e1420ba814204
Sean Herron <seanherron@gmail.com>
2014-03-11 00:26:10 -0400

    testing

--- recalls/recalls.py
+++ recalls/recalls.py
@@ -19,7 +19,8 @@ es = ElasticSearch(os.environ['ES_URL'])
 def show_splash():
         platform = request.user_agent.platform
         if platform == 'android' or platform == 'iphone' or platform == 'ipad':
-            query_url = 'zxing://scan/?ret=%s{CODE}' % urllib.quote_plus(request.url_root)
+            query_url = 'blah'
+            
         else:
             query_url = None
 
