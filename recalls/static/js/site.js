63027b35a4758e10bae4bc26b6dc10c305d573df
Sean Herron <seanherron@gmail.com>
2014-03-11 13:51:55 -0400

    moved iosscan function

--- recalls/static/js/site.js
+++ recalls/static/js/site.js
@@ -7,14 +7,13 @@ $(document).ready(function() {
 		location.href = document.getElementsByClassName("barcode-button")[0].getElementsByTagName("a")[0].href;
 		}, 500);
 	}
-
-  function iOSscan(url) {
-    setTimeout(function() {
-      // if pic2shop not installed yet, go to App Store
-      window.location = "http://itunes.com/apps/pic2shop";
-    }, 25);
-    // launch pic2shop and tell it to open Google Products with scan result
-    window.location=url;
-  }
-
 });
+
+function iOSscan(url) {
+  setTimeout(function() {
+    // if pic2shop not installed yet, go to App Store
+    window.location = "http://itunes.com/apps/pic2shop";
+  }, 25);
+  // launch pic2shop and tell it to open Google Products with scan result
+  window.location=url;
+}
