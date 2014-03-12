786449709bf369fe585b742a60944ad1e5a7fa28
Sean Herron <seanherron@gmail.com>
2014-03-12 11:59:23 -0400

    added formats

--- recalls/recalls.py
+++ recalls/recalls.py
@@ -19,8 +19,9 @@ def generate_query_url(platform):
     if platform == 'android':
         query_url = 'zxing://scan/?ret=%ssearch?q={CODE}' % urllib.quote_plus(request.url_root)
     elif platform == 'iphone' or platform == 'ipad':
-        query_url = 'pic2shop://scan?callback=%ssearch?q=EAN' % urllib.quote_plus(request.url_root)
+        query_url = 'p2spro://configure?formats=UPCE,UPC&lookup=%s/CODE' % urllib.quote_plus(request.url_root)
     else:
+        
         query_url = None
     
     return query_url
